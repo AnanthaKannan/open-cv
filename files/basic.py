8cf3f2241c58ec8a8f8add55d40433086776bdeb
SreeAnanthaKannan <sreeananthakannan@gmail.com>
2020-01-04 22:13:58 +0530

    first commit

--- files/basic.py
+++ files/basic.py
@@ -1,3 +1,18 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
+path = "../images/first.jpg"
+# to read the image
+input = cv.imread(path)
+# find the shape
+print(input.shape)
+print('Height', int(input.shape[0]), 'px')
+print('Height', int(input.shape[1]), 'px')
+# to show the image
+window_title = "Hello world"
+cv.imshow(window_title, input)
+# waitKey used for click any button and the window will destroy
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+cv.imwrite('output.jpg', input)
